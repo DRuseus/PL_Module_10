54eea9c6c1f9cc8b7f6ecab685b6b1b7875f8eab
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-09-16 23:22:41 +0300

    Выполнено практическое задание по теме "Многопроцессное программирование"

--- Module_10_5/module_10_5.py
+++ Module_10_5/module_10_5.py
@@ -1,65 +1,29 @@
 import multiprocessing
 from datetime import datetime
-from threading import Thread
 
 
 def read_info(name):
     with open(name, 'r') as file:
-        all_data = []
-        file.seek(0)
-        while True:
-            line = file.readline().rstrip('\n')
-            if not line:
-                break
-            all_data.append(line)
-            print(line)
-
-        """for line in file.readlines():
-            if not line.isspace():
-                print(line.rstrip('\n'))
-                all_data.append(line.rstrip('\n'))"""
-
-        """all_data = [line.rstrip('\n') for line in file.readlines() if not line.isspace()]"""  # Это если не печатать
+        all_data = [line[0:-1] for line in file.readlines() if not line.isspace()]
 
 
-file_list = (
-    'file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt'
-)
-
-thread_1 = Thread(target=read_info, args=(file_list[0], ))
-thread_2 = Thread(target=read_info, args=(file_list[1], ))
-thread_3 = Thread(target=read_info, args=(file_list[2], ))
-thread_4 = Thread(target=read_info, args=(file_list[3], ))
+file_list = [f'./file {number}.txt' for number in range(1, 5)]
 
 if __name__ == '__main__':
     # Линейное вычисление
-    start_0 = datetime.now()
+    start = datetime.now()
     read_info('file 1.txt')
     read_info('file 2.txt')
     read_info('file 3.txt')
     read_info('file 4.txt')
-    end_0 = datetime.now()
+    end = datetime.now()
+    print(end - start)
+    # 0:00:04.380398
 
     # Мультипроцессорное вычисление
     with multiprocessing.Pool(processes=4) as pool:
-        start_1 = datetime.now()
+        start = datetime.now()
         pool.map(read_info, file_list)
-    end_1 = datetime.now()
-
-    # Просто для сравнение Мултипоточное вычисление
-    start_2 = datetime.now()
-    thread_1.start()
-    thread_2.start()
-    thread_3.start()
-    thread_4.start()
-
-    thread_1.join()
-    thread_2.join()
-    thread_3.join()
-    thread_4.join()
-    end_2 = datetime.now()
-
-    # Вывод времени вычислений
-    print(end_0 - start_0)
-    print(end_1 - start_1)
-    print(end_2 - start_2)
+    end = datetime.now()
+    print(end - start)
+    # 0:00:01.683522
